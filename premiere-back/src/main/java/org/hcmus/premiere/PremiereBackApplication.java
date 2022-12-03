00a2473347d7f828106b38b1218e0f53bf816e6d
Nguyen Duc Nam <namworkmc@gmail.com>
2022-12-03 21:48:02 +0700

    Init project

---
+++ premiere-back/src/main/java/org/hcmus/premiere/PremiereBackApplication.java
@@ -0,0 +1,13 @@
+package org.hcmus.premiere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremiereBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PremiereBackApplication.class, args);
+  }
+
+}
---
+++ premiere-back/src/test/java/org/hcmus/premiere/PremiereBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hcmus.premiere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremiereBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
