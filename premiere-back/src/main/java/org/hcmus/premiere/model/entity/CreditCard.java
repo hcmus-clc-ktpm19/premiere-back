9ad6d35a3a2f849ff02ca2c015f90ff0a2d6e267
minhnhat02122001 <lengocminhnhatntp@gmail.com>
2022-12-12 22:11:37 +0700

    [PREM-2] Edit database

--- premiere-back/src/main/java/org/hcmus/premiere/model/entity/CreditCard.java
+++ premiere-back/src/main/java/org/hcmus/premiere/model/entity/CreditCard.java
@@ -1,5 +1,6 @@
 package org.hcmus.premiere.model.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -7,9 +8,11 @@ import java.util.Set;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import lombok.Data;
 
@@ -30,22 +33,16 @@ public class CreditCard extends PremiereAbstractEntity {
   @Column(name = "card_number", nullable = false, unique = true, columnDefinition = "VARCHAR(255)")
   private String cardNumber;
 
-  @ManyToOne
+  @JsonIgnore
+  @OneToOne(fetch = FetchType.LAZY)
   @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
   private User user;
-
-  @ManyToOne
-  @JoinColumn(name = "bank_id", referencedColumnName = "id", nullable = false)
-  private Bank bank;
 
   @OneToMany(mappedBy = "senderCreditCard")
   private Set<LoanReminder> senderLoanReminders;
 
   @OneToMany(mappedBy = "receiverCreditCard")
   private Set<LoanReminder> receiverLoanReminders;
-
-  @OneToMany(mappedBy = "senderCreditCard")
-  private Set<Transaction> transactions;
 
   @Override
   public boolean equals(Object o) {
