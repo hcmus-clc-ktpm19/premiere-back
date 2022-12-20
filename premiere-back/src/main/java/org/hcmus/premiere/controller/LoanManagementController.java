798c75f5e45cc7ac5357bee849b3476547c0a8c9
hoanghuugiap241 <hoanghuugiap241@gmail.com>
2022-12-20 18:32:08 +0700

    [PREM-7] List all loan reminder from user credit card number

--- premiere-back/src/main/java/org/hcmus/premiere/controller/LoanManagementController.java
+++ premiere-back/src/main/java/org/hcmus/premiere/controller/LoanManagementController.java
@@ -2,13 +2,16 @@ package org.hcmus.premiere.controller;
 
 import static org.hcmus.premiere.model.consts.PremiereApiUrls.PREMIERE_API_V1;
 
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hcmus.premiere.model.dto.CreateLoanReminderDto;
+import org.hcmus.premiere.model.dto.LoanReminderDto;
 import org.hcmus.premiere.model.dto.UserDto;
 import org.hcmus.premiere.model.entity.LoanReminder;
 import org.hcmus.premiere.service.LoanReminderService;
 import org.hcmus.premiere.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,6 +31,16 @@ public class LoanManagementController extends AbstractApplicationController {
   @GetMapping("/user/card-number/{cardNumber}")
   public UserDto getDebtor(@PathVariable String cardNumber) {
     return userMapper.toDto(userService.findUserByCreditCardNumber(cardNumber));
+  }
+
+  @GetMapping("/loan-reminder/{userCreditCardNumber}")
+  public ResponseEntity<List<LoanReminderDto>> getLoanRemindersByUserCreditCardNumber(@PathVariable String userCreditCardNumber) {
+    return ResponseEntity.ok(
+        loanReminderService
+            .getLoanRemindersByUserCreditCardNumber(userCreditCardNumber)
+            .stream()
+            .map(applicationMapper::toLoanReminderDto)
+            .toList());
   }
 
   @PostMapping
--- premiere-back/src/main/java/org/hcmus/premiere/util/mapper/ApplicationMapper.java
+++ premiere-back/src/main/java/org/hcmus/premiere/util/mapper/ApplicationMapper.java
@@ -1,9 +1,11 @@
 package org.hcmus.premiere.util.mapper;
 
 import org.hcmus.premiere.model.dto.CreditCardDto;
+import org.hcmus.premiere.model.dto.LoanReminderDto;
 import org.hcmus.premiere.model.dto.OTPDto;
 import org.hcmus.premiere.model.dto.ReceiverDto;
 import org.hcmus.premiere.model.entity.CreditCard;
+import org.hcmus.premiere.model.entity.LoanReminder;
 import org.hcmus.premiere.model.entity.OTP;
 import org.hcmus.premiere.model.entity.Receiver;
 import org.springframework.stereotype.Component;
@@ -47,6 +49,23 @@ public class ApplicationMapper {
       otpDto.setEmail(otp.getEmail());
       otpDto.setCreatedAt(otp.getCreatedAt());
       return otpDto;
+    }
+  }
+
+  public LoanReminderDto toLoanReminderDto(LoanReminder loanReminder){
+    if (loanReminder == null) {
+      return null;
+    } else {
+      LoanReminderDto loanReminderDto = new LoanReminderDto();
+      loanReminderDto.setId(loanReminder.getId());
+      loanReminderDto.setVersion(loanReminder.getVersion());
+      loanReminderDto.setTransferAmount(loanReminder.getLoanBalance());
+      loanReminderDto.setStatus(loanReminder.getStatus());
+      loanReminderDto.setTime(loanReminder.getTime());
+      loanReminderDto.setLoanRemark(loanReminder.getLoanRemark());
+      loanReminderDto.setSenderCreditCardId(loanReminder.getSenderCreditCard().getId());
+      loanReminderDto.setReceiverCreditCardId(loanReminder.getReceiverCreditCard().getId());
+      return loanReminderDto;
     }
   }
 }
