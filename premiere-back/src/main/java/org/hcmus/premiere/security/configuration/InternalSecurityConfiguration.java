e2ebb1d593fc7e7c9aac5776846db9d2bde787c8
Nguyen Duc Nam <namworkmc@gmail.com>
2022-12-27 16:53:39 +0700

    [PREM-28] Separate security configs

---
+++ premiere-back/src/main/java/org/hcmus/premiere/security/configuration/InternalSecurityConfiguration.java
@@ -0,0 +1,61 @@
+package org.hcmus.premiere.security.configuration;
+
+import static org.hcmus.premiere.model.enums.PremiereRole.CUSTOMER;
+import static org.hcmus.premiere.model.enums.PremiereRole.EMPLOYEE;
+import static org.hcmus.premiere.model.enums.PremiereRole.PREMIERE_ADMIN;
+
+import org.hcmus.premiere.common.consts.PremiereApiUrls;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@Import(KeycloakSpringBootConfigResolver.class)
+@Order(1)
+public class InternalSecurityConfiguration extends KeycloakWebSecurityConfigurerAdapter {
+
+  @Autowired
+  public void configureGlobal(AuthenticationManagerBuilder auth) {
+    KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+    keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+    auth.authenticationProvider(keycloakAuthenticationProvider);
+  }
+
+  @Bean
+  @Override
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
+
+    http.cors()
+        .and()
+        .csrf()
+        .disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .antMatcher(PremiereApiUrls.PREMIERE_API_V1 + "/**")
+        .authorizeRequests()
+        .antMatchers(PremiereApiUrls.PREMIERE_API_V1 + "/credit-card/**")
+        .hasAnyRole(CUSTOMER.value, EMPLOYEE.value, PREMIERE_ADMIN.value)
+        .antMatchers(PremiereApiUrls.PREMIERE_API_V1 + "/receivers/**").hasRole(CUSTOMER.value)
+        .anyRequest()
+        .authenticated(); // used to be .permitAll()
+  }
+}
