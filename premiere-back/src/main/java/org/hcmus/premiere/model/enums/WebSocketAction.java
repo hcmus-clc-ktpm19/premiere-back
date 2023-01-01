e40e75fc2ab67f076d11bd9fa4bad53605326c49
Nguyen Duc Nam <namworkmc@gmail.com>
2023-01-01 14:39:19 +0700

    [PREM-16] Add deposit socket service

---
+++ premiere-back/src/main/java/org/hcmus/premiere/model/enums/WebSocketAction.java
@@ -0,0 +1,10 @@
+package org.hcmus.premiere.model.enums;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum WebSocketAction {
+  DEPOSIT_MONEY("DEPOSIT_MONEY");
+
+  final String value;
+}
