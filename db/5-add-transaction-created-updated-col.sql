0de367f4886ba10920a05370470f636fff1e6705
Nguyen Duc Nam <66512409+namworkmc@users.noreply.github.com>
2023-01-04 14:43:20 +0700

    [PREM-35] Add sample data, add base table (#28)

--- db/5-add-transaction-created-updated-col.sql
+++ db/5-add-transaction-created-updated-col.sql
@@ -1,11 +1,5 @@
 SET SEARCH_PATH TO premiere;
 
-ALTER TABLE "checking_transaction" ADD COLUMN "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "checking_transaction" ADD COLUMN "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-
 UPDATE "checking_transaction" SET "created_at" = NOW() - INTERVAL '1 DAY' * id, "updated_at" = NOW() + INTERVAL '1 DAY' * id;
-
-ALTER TABLE "transaction" ADD COLUMN "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE "transaction" ADD COLUMN "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 
 UPDATE "transaction" SET "created_at" = NOW() - INTERVAL '1 DAY' * id, "updated_at" = NOW() + INTERVAL '1 DAY' * id;
