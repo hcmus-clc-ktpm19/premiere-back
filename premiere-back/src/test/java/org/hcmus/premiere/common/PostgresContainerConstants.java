26de05eefcb2e99d4e6b718f6ecc47f0ac674cb0
namworkmc <namworkmc@gmail.com>
2023-01-26 16:21:52 +0700

    Change @UtilityClass to manual static fields

--- premiere-back/src/test/java/org/hcmus/premiere/common/PostgresContainerConstants.java
+++ premiere-back/src/test/java/org/hcmus/premiere/common/PostgresContainerConstants.java
@@ -1,15 +1,12 @@
 package org.hcmus.premiere.common;
 
-import lombok.experimental.UtilityClass;
-
-@UtilityClass
 public class PostgresContainerConstants {
 
-  public final String POSTGRES_CONTAINER_IMAGE = "postgres:15.1-alpine3.17";
-  public final String POSTGRES_DATABASE_NAME = "PREMIERE";
-  public final String POSTGRES_USERNAME = "admin";
-  public final String POSTGRES_PASSWORD = "admin";
-  public final int POSTGRES_PORT = 5432;
-  public final String DB_SCRIPTS_PATH = "src/test/resources/db";
-  public final String ENTRYPOINT_INIT_DB_PATH = "/docker-entrypoint-initdb.d";
+  public static final String POSTGRES_CONTAINER_IMAGE = "postgres:15.1-alpine3.17";
+  public static final String POSTGRES_DATABASE_NAME = "PREMIERE";
+  public static final String POSTGRES_USERNAME = "admin";
+  public static final String POSTGRES_PASSWORD = "admin";
+  public static final int POSTGRES_PORT = 5432;
+  public static final String DB_SCRIPTS_PATH = "src/test/resources/db";
+  public static final String ENTRYPOINT_INIT_DB_PATH = "/docker-entrypoint-initdb.d";
 }
